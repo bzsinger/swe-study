c029d800df4679fcf638d50b5d382f3bafadc1ba
bzsinger <benjamin.z.singer@gmail.com>
2018-04-29 14:34:25 -0500

    Add unpacking

--- test-2/javascript/functions.js
+++ test-2/javascript/functions.js
@@ -8,6 +8,8 @@ function f(x, y, z=3) {   // if no argument passed in for z, uses value 3
 f(1, 2, 4)  // [1, 2, 4]
 f(1, 2)     // [1, 2, 3]
 
+// ------------------------------------------------------------------------
+
 // middle argument
 function g(x, y=3, z) {  // in JavaScript, middle argument can have default
     return [x, y, z]
@@ -18,6 +20,8 @@ g();        // [undefined, 5, undefined]
 g(1, 2, 4); // [1, 2, 4]
 g(1, 2);    // [1, 2, undefined]
 g(1);       // [1, 3, undefined]
+
+// ------------------------------------------------------------------------
 
 // mutables
 function h(x=[]) {      // uses a new array each time
--- test-2/python/unpacking.py
+++ test-2/python/unpacking.py
@@ -7,7 +7,7 @@ def f(x, y, z):
 
 t = (3, 4)
 f(2, *t)    # [2, 3, 4]
-f(*t, 2)    # [3, 4, 2]
+f(*t, 2)    # [3, 4, 2] - can do unpacking before position
 
 # f(x=2, *t)    # * has higher precedence than pass by name, gets multiple
                 #   values for x
@@ -19,3 +19,11 @@ f(*t, 2)    # [3, 4, 2]
 
 d = {"z": 4, "y": 3, "x": 2}
 f(**d)     # [2, 3, 4]
+
+# f(x=2, **d)   # gets conflicting values for x
+
+e = {"z": 4, "y": 3}
+f(2, **e)  # because no x in dict, can consume first argument by position
+
+# f(**d, 2)         # cannot unpack dictionary before position
+# f(**d, *t, y=2)   # cannot unpack dictionary before iterable unpacking
