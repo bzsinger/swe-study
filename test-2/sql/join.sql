da799e2efc93c9ee9e6df1aef6ad48716e91e92f
bzsinger <benjamin.z.singer@gmail.com>
2018-04-30 16:12:58 -0500

    Added join

---
+++ test-2/sql/join.sql
@@ -0,0 +1,107 @@
+/* join */
+
+/* assume tables:
+Student
+  sID     int not null, primary key
+  sName   text
+  GPA     float
+
+Apply
+  sID        int
+  cName      varchar(8)
+  major      varchar(255)
+  decision   varchar(4)
+
+  foreign key(sID) references Student(sID)
+*/
+
+/* ------------------------------------------------------------------------ */
+
+/* cross join
+    - takes: table, table
+    - produces: table
+*/
+
+select *
+  from Student
+  cross join Apply
+  order by Student.id
+
+  /* takes every student, matches each row of Apply table to every student
+        (even if matching doesn't make sense) */
+
+/* if table A has x rows, and table B has y rows, A cross join B has
+        x * y rows */
+
+/* ------------------------------------------------------------------------ */
+
+/* theta join
+    - takes: table, table, predicate (identify matching attributes)
+    - produces: table
+*/
+
+select *
+  from Student
+  inner join Apply
+  on Student.sID = Apply.sID
+  /* sets which attributes should be related in joins
+        tolerates different field names (ex. if Student's id was named 'id') */
+
+select *
+  from Student
+  inner join Apply (using sID)
+  /* only usable if use same name for both fields */
+  /* only returns rows that match */
+
+/* if table A has x rows, and table B has y rows, A inner join B has
+      at most max(x, y) rows */
+
+/* ------------------------------------------------------------------------ */
+
+/* natural join
+    - takes: table, table
+    - produces: table
+*/
+
+select *
+  from Student
+  natural join Apply
+  /* joins on any matching field in both tables */
+
+  /* if no attributes match, returns *cross join*
+      if an attribute matches, returns only rows with matching values for those
+        attributes */
+
+/* ------------------------------------------------------------------------ */
+
+/* since join produces a table, can use 'where' to do a select and replace
+      * with column names to do a project */
+
+select sName, GPA
+  from Student
+  inner join Apply (using sID)
+  where (GPA > 3.0) and
+        (sID < 200)
+
+/* can do multiple joins in single SQL statement */
+
+select *
+  from Student
+    inner join Apply (using sID)
+    inner join Apply (using cName)
+  where
+    (sizeHS > 1000)     and
+    (major = "CS")      and
+    (decision = "true") and
+    (enrollment > 20000)
+
+/* ------------------------------------------------------------------------ */
+
+/* if doing self join, need to rename tables */
+
+select R.cName, R.state
+  from College as R
+    inner join College as S
+  where (R.cName != S.cName) and
+        (R.state = S.state)
+        /* only returns college if state has > 1 college */
